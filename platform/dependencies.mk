52fb8927a97470562be887f356e60390cc3ffae9
Alexander Grund <Flamefire@users.noreply.github.com>
2022-11-17 17:27:56 +0100

    Revert "treble: Create system copy of ims-ext-common and add to classpath"
    This reverts commit 6c36f18a159da1885d1f4501edd4f2d79ed6ddb3.
    
    No longer required with the SLD_sprout ims.apk
    
    Change-Id: Ia5bd1bdac1feeeed3c3eb7fcae24bd16c530c9d0

--- platform/dependencies.mk
+++ platform/dependencies.mk
@@ -55,11 +55,7 @@ PRODUCT_PACKAGES += \
 ### IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
-    ims-ext-common_system \
     ims_ext_common.xml
-
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
 
 ### IPV6
 PRODUCT_PACKAGES += \
